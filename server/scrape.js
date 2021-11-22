cf1a3fdcfcddc821cb118c93186c31ec569d1e8d
slater.gordon <slater.gordon@designitcorp.ca>
2021-11-21 18:14:46 -0800

    scrape based on area now

--- server/scrape.js
+++ server/scrape.js
@@ -1,51 +1,60 @@
 import axios from 'axios';
+import querystring from 'querystring';
 
 function genRand(min, max, decimalPlaces) {
-    var rand = Math.random() * (max - min) + min;
-    var power = Math.pow(10, decimalPlaces);
-    return Math.floor(rand * power) / power;
+	var rand = Math.random() * (max - min) + min;
+	var power = Math.pow(10, decimalPlaces);
+	return Math.floor(rand * power) / power;
 }
 
 async function post(climb) {
-    try {
-        await axios.post('http://localhost:5000/climbs', {climb: climb});
-    } catch(err) {
-        console.log(err);
-    }
+	try {
+		await axios.post('http://localhost:5000/climbs', { climb: climb });
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 let promises = [];
 let climbs = [];
 
 try {
-    for (let i = 40; i < 100; i++) {
-        promises.push(axios
-            .post('https://sendage.com/api/climbs', { page: i })
-            .then(res => {
-                res.data.climbs?.map((climb) => {
-                    if (climb.Climb.name && climb.Climb.type == 'boulder') {
-                        climbs.push({
-                            name: climb.Climb.name,
-                            grade: "v" + String((Number(climb.Climb.grade_id) - 4) / 6).slice(0, 1),
-                            description: "",
-                            quality: genRand(0, 5, 1),
-                            area: climb.Climb.area
-                        });
-                    }
-                })
-            })
-            .catch(error => {
-                console.error(error)
-            })
-        );
-    }
+	axios
+		.post('https://sendage.com/api/climbs', querystring.stringify({ 'page': 1, 'areas[]': 5947, 'area_parents': false, 'limit': 500 }))
+		.then(res => {
+			res.data.climbs?.map((climb) => {
+				console.log(climb);
+			})
+		})
+		.catch(error => {
+			console.error(error)
+		})
+	// for (let i = 40; i < 100; i++) {
+	// 	promises.push(axios
+	// 		.post('https://sendage.com/api/climbs', {page: 1})
+	// 		.then(res => {
+	// 			res.data.climbs?.map((climb) => {
+	// 				climbs.push({
+	// 					name: climb.Climb.name,
+	// 					grade: "v" + String((Number(climb.Climb.grade_id) - 4) / 6).slice(0, 1),
+	// 					description: "",
+	// 					quality: genRand(0, 5, 1),
+	// 					area: climb.Climb.area_id
+	// 				});
+	// 			})
+	// 		})
+	// 		.catch(error => {
+	// 			console.error(error)
+	// 		})
+	// 	);
+	// }
 
-    let more = [];
-    Promise.all(promises).then(() => {
-        climbs.map((climb) => {
-            more.push(post(climb));
-        })
-    });
+	// let more = [];
+	// Promise.all(promises).then(() => {
+	// 	climbs.map((climb) => {
+	// 		console.log(climb);
+	// 	})
+	// });
 } catch (err) {
-    console.log(err);
+	console.log(err);
 }
