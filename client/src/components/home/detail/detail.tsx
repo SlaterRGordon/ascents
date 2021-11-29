54623bf8193bfc3be07dfd2a2e78b4779305f731
SlaterRGordon <slats1999@gmail.com>
2021-11-29 15:22:44 -0800

    load climb object into detail

--- client/src/components/home/detail/detail.tsx
+++ client/src/components/home/detail/detail.tsx
@@ -3,6 +3,11 @@ import './detail.css';
 import { IconButton, Toolbar } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
+import { useEffect, useState } from 'react';
+import { useSelector, RootStateOrAny, useDispatch } from 'react-redux';
+
+import { getClimb } from '../../../flux/actions/climbs';
+
 
 interface DetailProps {
 	id: string;
@@ -12,21 +17,48 @@ interface DetailProps {
 const Detail = (props: DetailProps) => {
 	const { id, closeItem } = props;
 
+	const { climb } = useSelector((state: RootStateOrAny) => state.climbs);
+
+	const [loading, setLoading] = useState(false);
+
+	const dispatch = useDispatch();
+
+	// Hook on new id
+	useEffect(() => {
+		const loadClimb = async () => {
+			await dispatch(getClimb(id));
+			setLoading(false);
+		};
+
+		if (!loading) {
+			setLoading(true);
+			loadClimb();
+		}
+	}, [id]);
 
 	return (
 		<>
-		<Toolbar />
-		<div className="detail">
-			<div className="detailHeader">
-				<div>{id}</div>
-				<IconButton onClick={closeItem}>
-					<CloseIcon className="headerIcon" />
-				</IconButton>
-			</div>
-			<div className="detailBody">
-				Detail body content here...
-			</div>
-		</div>
+			{climb ?
+				<>
+					<Toolbar />
+					<div className="detail">
+						<div className="detailHeader">
+							<div>{climb.name}</div>
+							<IconButton onClick={closeItem}>
+								<CloseIcon className="headerIcon" />
+							</IconButton>
+						</div>
+						<div className="detailBody">
+							<div>Detail body content here...</div>
+							<div className="ascents">
+								
+							</div>
+						</div>
+					</div>
+				</>
+				:
+				<></>
+			}
 		</>
 	);
 }
